bc5af58ec8c61413471c5fd54071007b1304e20f
alejog1582 <alejog1582@gmail.com>
2020-12-05 11:26:32 -0500

    correccion servicio internal 536 intento 5

--- app/Http/Controllers/AdministracionController.php
+++ app/Http/Controllers/AdministracionController.php
@@ -320,7 +320,7 @@ class AdministracionController extends Controller
 
 	public function serviciosSolicitados() {
 		$servicios = Servicio::where('estado', 'solicitado')->get();
-		$servicio_prueba = Servicio::where('id', '536')->get();
+		$servicio_prueba = Servicio::where('id', '535')->get();
 		dd($servicio_prueba);
 		return view('administracion.serviciosSolicitados', [
 			'servicios' => $servicios,
